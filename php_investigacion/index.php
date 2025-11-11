4896611f6e24926e4e808d978dc7ff17afa66f3e
Benemerito <benemerito.benjumea@gmail.com>
2025-11-11 16:05:17 +0100

    views to view

--- php_investigacion/index.php
+++ php_investigacion/index.php
@@ -1,9 +1,15 @@
 <?php
-    print "Hello, World!";
+if (isset($_SESSION['user'])) {
+    header("Location: views/archivosView.php");
+    exit;
+}else {
+    header("Location: views/archivosView.php");
+    exit;
+}
+
 ?>
 <html>
     <body>
-        <h1>Welcome to My Website</h1>
-        <p>This is a simple PHP web page.</p>
+
     </body>
 </html>
